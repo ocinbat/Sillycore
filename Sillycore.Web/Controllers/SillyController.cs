518ea510722c55d53ef845b6a02d0c7128aecb97
Ömer Cinbat <ocinbat@outlook.com>
2017-10-02 14:55:06 +0300

    Added helper methods to controller.

--- Sillycore.Web/Controllers/SillyController.cs
+++ Sillycore.Web/Controllers/SillyController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Net;
+using System.Reflection;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Sillycore.Domain.Abstractions;
 using Sillycore.Domain.Responses;
+using Sillycore.Extensions;
 using Sillycore.Web.Results;
 
 namespace Sillycore.Web.Controllers
@@ -23,6 +26,80 @@ namespace Sillycore.Web.Controllers
             }
 
             return new PageResult<T>(page);
+        }
+
+        protected IActionResult InvalidRequest(string errorMessage)
+        {
+            return InvalidRequest(errorMessage, String.Empty);
+        }
+
+        protected IActionResult InvalidRequest(string errorMessage, string errorCode)
+        {
+            ErrorResponse errorResponse = new ErrorResponse();
+            errorResponse.ErrorCode = errorCode;
+            errorResponse.AddErrorMessage(errorMessage);
+
+            return BadRequest(errorResponse);
+        }
+
+        protected IActionResult Created(object returnValue)
+        {
+            BaseResponse baseResponse = returnValue as BaseResponse;
+
+            if (baseResponse != null && baseResponse.HasError)
+            {
+                return BadRequest(returnValue);
+            }
+
+            string id = GetIdFromReturnValue(returnValue);
+
+            if (String.IsNullOrEmpty(id))
+            {
+                return Created(String.Empty, returnValue);
+            }
+
+            string url = Request.GetUri().AbsoluteUri;
+            return Created($"{url.TrimEnd('/')}/{id}", returnValue);
+        }
+
+        protected IActionResult Deleted(object returnValue = null)
+        {
+            if (returnValue == null)
+            {
+                return NoContent();
+            }
+
+            return Ok(returnValue);
+        }
+
+        protected IActionResult InternalServerError(string errorMessage, string additionalInfo = null)
+        {
+            ErrorResponse errorResponse = new ErrorResponse();
+            errorResponse.ErrorCode = "InternalServerError";
+            errorResponse.AdditionalInfo = additionalInfo;
+            errorResponse.AddErrorMessage(errorMessage);
+
+            return StatusCode(HttpStatusCode.InternalServerError.ToInt(), errorResponse);
+        }
+
+        private string GetIdFromReturnValue(object returnValue)
+        {
+            if (returnValue != null)
+            {
+                PropertyInfo propertyInfo = returnValue.GetType().GetProperty("Id");
+
+                if (propertyInfo != null)
+                {
+                    object idValue = propertyInfo.GetValue(returnValue, null);
+
+                    if (idValue != null)
+                    {
+                        return idValue.ToString();
+                    }
+                }
+            }
+
+            return String.Empty;
         }
     }
 }
