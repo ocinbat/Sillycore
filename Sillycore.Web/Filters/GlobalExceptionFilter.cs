1b246fd21059b43b3ddeed8c9a46f409e7aac5cf
Ömer Cinbat <ocinbat@outlook.com>
2017-10-02 15:23:07 +0300

    Created global exception handler.

---
+++ Sillycore.Web/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Sillycore.Domain.Responses;
+
+namespace Sillycore.Web.Filters
+{
+    public class GlobalExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            ErrorResponse errorResponse = new ErrorResponse();
+            errorResponse.AdditionalInfo = context.Exception?.Message;
+            errorResponse.ErrorCode = "InternalServerError";
+            // TODO Bunu resource'a çıkar.
+            errorResponse.AddErrorMessage("There was a problem while processing your request.");
+
+            var objectResult = new ObjectResult(errorResponse);
+            objectResult.StatusCode = 500;
+
+            context.Result = objectResult;
+        }
+    }
+}
