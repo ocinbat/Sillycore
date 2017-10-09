4cad86d51dd5fa47e21fc0822a78c4bbde22c5bd
Ömer Cinbat <ocinbat@outlook.com>
2017-10-09 14:58:29 +0300

    GlobalExceptionHandler logger fix.

--- Sillycore.Web/Filters/GlobalExceptionFilter.cs
+++ Sillycore.Web/Filters/GlobalExceptionFilter.cs
@@ -1,17 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 using Sillycore.Domain.Responses;
 
 namespace Sillycore.Web.Filters
 {
     public class GlobalExceptionFilter : ExceptionFilterAttribute
     {
+        private readonly ILogger<GlobalExceptionFilter> _logger;
+
+        public GlobalExceptionFilter(ILogger<GlobalExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
         public override void OnException(ExceptionContext context)
         {
             while (context.Exception?.InnerException != null)
             {
                 context.Exception = context.Exception.InnerException;
             }
+
+            _logger.LogError(context.Exception, "There was a problem while processing your request.");
 
             ErrorResponse errorResponse = new ErrorResponse();
             errorResponse.AdditionalInfo = context.Exception?.Message;
