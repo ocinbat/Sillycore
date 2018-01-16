d25d905a04422a43dbd61314a0318c8563166768
Metehan Şenol <metehansenol@gmail.com>
2018-01-16 14:45:10 +0300

    TContext added to DI service collection

--- Sillycore.EntityFramework/SillycoreAppBuilderExtensions.cs
+++ Sillycore.EntityFramework/SillycoreAppBuilderExtensions.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Sillycore.EntityFramework
 {
     public static class SillycoreAppBuilderExtensions
     {
-        
+        public static SillycoreAppBuilder UseEntityFramework<TContext>(this SillycoreAppBuilder builder, Action<DbContextOptionsBuilder> dbContextOptionsBuilder)
+            where TContext : DbContext
+        {
+            builder.Services.AddDbContext<TContext>(dbContextOptionsBuilder);
+
+            return builder;
+        }
     }
 }
