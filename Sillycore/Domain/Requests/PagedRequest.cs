9badf523dd09fada1600427342d1810087fa46aa
Metehan Şenol <metehansenol@gmail.com>
2018-01-16 14:53:00 +0300

    PagedRequest object added to Sillycore domain objects

---
+++ Sillycore/Domain/Requests/PagedRequest.cs
@@ -0,0 +1,12 @@
+﻿using Sillycore.Domain.Enums;
+
+namespace Sillycore.Domain.Requests
+{
+    public class PagedRequest
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public OrderType Order { get; set; }
+        public string OrderBy { get; set; }
+    }
+}
