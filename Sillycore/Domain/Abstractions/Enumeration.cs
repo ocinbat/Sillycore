32e982e69c426ff7c191237e3992bedd6e6bf217
Ömer Cinbat <ocinbat@outlook.com>
2018-01-19 14:15:18 +0300

    Enumeration class is added to Sillycore.

---
+++ Sillycore/Domain/Abstractions/Enumeration.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Sillycore.Domain.Abstractions
+{
+    public abstract class Enumeration : IComparable
+    {
+        public string Name { get; private set; }
+        public int Id { get; private set; }
+
+        protected Enumeration()
+        {
+        }
+
+        protected Enumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        private static IEnumerable<T> GetAll<T>() where T : Enumeration, new()
+        {
+            var type = typeof(T);
+            var fields = type.GetTypeInfo().GetFields(BindingFlags.Public |
+                                                      BindingFlags.Static |
+                                                      BindingFlags.DeclaredOnly);
+            foreach (var info in fields)
+            {
+                var instance = new T();
+                var locatedValue = info.GetValue(instance) as T;
+                if (locatedValue != null)
+                {
+                    yield return locatedValue;
+                }
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherValue = obj as Enumeration;
+            if (otherValue == null)
+            {
+                return false;
+            }
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Id.Equals(otherValue.Id);
+            return typeMatches && valueMatches;
+        }
+
+        public int CompareTo(object other)
+        {
+            return Id.CompareTo(((Enumeration)other).Id);
+        }
+    }
+}
