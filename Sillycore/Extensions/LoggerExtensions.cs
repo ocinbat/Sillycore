b345adb99a5f5c17668b14927601b99589d9fae5
Ömer Cinbat <ocinbat@outlook.com>
2018-01-23 14:47:41 +0300

    ExecutionTime logger extension method implemented.

---
+++ Sillycore/Extensions/LoggerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Sillycore.Logging;
+
+namespace Sillycore.Extensions
+{
+    public static class LoggerExtensions
+    {
+        public static LogContext LogExecutionTime(this ILogger logger, string message = "")
+        {
+            LogContext context = new LogContext(logger);
+
+            if (!String.IsNullOrWhiteSpace(message))
+            {
+                context.WithMessage(message);
+            }
+
+            return context.WithExecutionTime();
+        }
+    }
+}
---
+++ Sillycore/Logging/LogContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Sillycore.Extensions;
+
+namespace Sillycore.Logging
+{
+    public class LogContext : IDisposable
+    {
+        private readonly ILogger _logger;
+        private string _message;
+        private bool _logExecutionTime = false;
+        private readonly bool _logExecutionTimeGlobal = SillycoreApp.Instance.Configuration["LogExecutionTime"].ToBool();
+
+        private readonly Stopwatch _sw;
+
+        public LogContext(ILogger logger)
+        {
+            _logger = logger;
+            _sw = Stopwatch.StartNew();
+        }
+
+        public LogContext WithMessage(string message)
+        {
+            _message = message;
+            return this;
+        }
+
+        public LogContext WithExecutionTime()
+        {
+            _logExecutionTime = true;
+            return this;
+        }
+
+        public void Dispose()
+        {
+            if (_logExecutionTime || _logExecutionTimeGlobal)
+            {
+                _sw.Stop();
+                long elapsedMs = _sw.ElapsedMilliseconds;
+                _logger.LogInformation($"{_message} took {elapsedMs} miliseconds");
+            }
+            else
+            {
+                _logger.LogInformation($"{_message}");
+            }
+        }
+    }
+}
