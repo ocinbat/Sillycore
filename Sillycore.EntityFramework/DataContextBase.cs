689c482f82bf88be745a6f853e2fc4f16a11ff7f
Ömer Cinbat <ocinbat@outlook.com>
2018-01-29 14:05:13 +0300

    Fix dbcontext options injection.

--- Sillycore.EntityFramework/DataContextBase.cs
+++ Sillycore.EntityFramework/DataContextBase.cs
@@ -6,6 +6,11 @@ namespace Sillycore.EntityFramework
 {
     public abstract class DataContextBase : DbContext
     {
+        protected DataContextBase(DbContextOptions options)
+            : base(options)
+        {
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.AddEntityConfigurationsFromAssembly(GetType().GetTypeInfo().Assembly);
