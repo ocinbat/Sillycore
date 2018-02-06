708f7291387e9f47dffc03d0bfd5b1fe4911307b
Kutlu Araslı <kutlu.arasli@trendyol.com>
2018-02-06 14:46:48 +0300

    Add rest client

---
+++ Sillycore.RestClient/Serialization/CustomJsonSerializer.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Deserializers;
+using RestSharp.Serializers;
+using Sillycore;
+
+namespace Sillycore.RestClient.Serialization
+{
+    public class CustomJsonSerializer : ISerializer, IDeserializer
+    {
+        public string Serialize(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public T Deserialize<T>(IRestResponse response)
+        {
+            return JsonConvert.DeserializeObject<T>(response.Content, SillycoreApp.JsonSerializerSettings);
+        }
+
+        public string RootElement { get; set; }
+        public string Namespace { get; set; }
+        public string DateFormat { get; set; }
+        public string ContentType
+        {
+            get { return "application/json"; }
+            set { }
+        }
+    }
+}
