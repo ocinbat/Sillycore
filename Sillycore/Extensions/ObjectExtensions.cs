a8fb4ab7d20dcb162b315c725ed41a206434bac9
Ömer Cinbat <ocinbat@outlook.com>
2018-03-05 14:51:49 +0300

    Added ToJson extensions to object.

--- Sillycore/Extensions/ObjectExtensions.cs
+++ Sillycore/Extensions/ObjectExtensions.cs
@@ -1,10 +1,17 @@
-﻿namespace Sillycore.Extensions
+﻿using Newtonsoft.Json;
+
+namespace Sillycore.Extensions
 {
     public static class ObjectExtensions
     {
         public static bool HasProperty(this object obj, string propertyName)
         {
             return obj.GetType().GetProperty(propertyName) != null;
+        }
+
+        public static string ToJson(this object obj)
+        {
+            return JsonConvert.SerializeObject(obj, SillycoreApp.JsonSerializerSettings);
         }
     }
 }
