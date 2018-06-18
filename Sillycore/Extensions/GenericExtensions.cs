69fcbc268f11e2026db2b74707fec3181f9081d1
Ömer Cinbat <ocinbat@outlook.com>
2018-06-18 17:05:05 +0300

    Clone fix.

--- Sillycore/Extensions/GenericExtensions.cs
+++ Sillycore/Extensions/GenericExtensions.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
 
 namespace Sillycore.Extensions
 {
@@ -16,24 +14,12 @@ namespace Sillycore.Extensions
 
         public static T Clone<T>(this T source)
         {
-            if (!typeof(T).IsSerializable)
-            {
-                throw new ArgumentException("The type must be serializable.", "source");
-            }
-
             if (Object.ReferenceEquals(source, null))
             {
                 return default(T);
             }
 
-            IFormatter formatter = new BinaryFormatter();
-            
-            using (Stream stream = new MemoryStream())
-            {
-                formatter.Serialize(stream, source);
-                stream.Seek(0, SeekOrigin.Begin);
-                return (T)formatter.Deserialize(stream);
-            }
+            return JsonConvert.DeserializeObject<T>(source.ToJson(), SillycoreApp.JsonSerializerSettings);
         }
     }
 }
