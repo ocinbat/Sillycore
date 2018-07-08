3f9c0b87c11e957d13235abbd770bba7faf0f7d4
Ömer Cinbat <ocinbat@outlook.com>
2018-07-08 11:24:58 +0300

    Generic config bind extension.

---
+++ Sillycore/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Sillycore.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Attempts to bind the given object type to the configuration section specified by the key by matching property names against configuration keys recursively.
+        /// </summary>
+        /// <param name="configuration">The configuration instance to bind.</param>
+        /// <param name="key">The key of the configuration section to bind.</param>
+        public static T Bind<T>(this IConfiguration configuration, string key)
+        {
+            T type = Activator.CreateInstance<T>();
+            configuration.Bind(key, type);
+            return type;
+        }
+    }
+}
