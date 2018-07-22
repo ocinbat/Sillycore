d1f094f0dc1fa7fd57ed456162923ea475182fbe
Ömer Cinbat <ocinbat@outlook.com>
2018-07-22 14:29:15 +0300

    Static file config implementation.

--- Sillycore.Web/Routing/StaticFileConfigurator.cs
+++ Sillycore.Web/Routing/StaticFileConfigurator.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Sillycore.Web.Abstractions;
 
 namespace Sillycore.Web.Routing
@@ -11,7 +12,11 @@ namespace Sillycore.Web.Routing
         public int Order => 10502;
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configuration, IServiceProvider serviceProvider)
         {
-            app.UseStaticFiles();
+            StaticFilesConfiguration staticFilesConfiguration = app.ApplicationServices.GetService<StaticFilesConfiguration>();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                ServeUnknownFileTypes = staticFilesConfiguration.ServeUnknownFileTypes
+            });
         }
     }
 }
---
+++ Sillycore.Web/Routing/StaticFilesConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using Sillycore.Configuration;
+
+namespace Sillycore.Web.Routing
+{
+    [Configuration("Sillycore:Web:StaticFiles")]
+    public class StaticFilesConfiguration
+    {
+        public bool ServeUnknownFileTypes { get; set; } = false;
+    }
+}
