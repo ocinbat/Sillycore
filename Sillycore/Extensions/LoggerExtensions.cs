6877894ba7af7a09f6e51d334c46d989995bb492
Ömer Cinbat <ocinbat@outlook.com>
2018-12-04 10:03:49 +0300

    Added threshold support

--- Sillycore/Extensions/LoggerExtensions.cs
+++ Sillycore/Extensions/LoggerExtensions.cs
@@ -6,7 +6,7 @@ namespace Sillycore.Extensions
 {
     public static class LoggerExtensions
     {
-        public static LogContext LogExecutionTime(this ILogger logger, string message = "")
+        public static LogContext LogExecutionTime(this ILogger logger, string message = "", int thresholdInMs = 0)
         {
             LogContext context = new LogContext(logger);
 
