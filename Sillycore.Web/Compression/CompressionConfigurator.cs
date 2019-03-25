b45f6532837ce483b4e246ced54b85906a86a605
Ömer Cinbat <ocinbat@outlook.com>
2019-03-25 08:54:25 +0300

    Added response compression to web package

---
+++ Sillycore.Web/Compression/CompressionConfigurator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sillycore.Abstractions;
+using Sillycore.Web.Abstractions;
+
+namespace Sillycore.Web.Compression
+{
+    public class CompressionConfigurator : IServiceConfigurator, IApplicationConfigurator
+    {
+        public void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddResponseCompression();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configuration,
+            IServiceProvider serviceProvider)
+        {
+            app.UseResponseCompression();
+        }
+
+        public int Order => 10002;
+    }
+}
